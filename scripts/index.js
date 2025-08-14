e44b5cb3de951b793df96e4f003a6d33e94977f1
reem moustapha <reemmoustapha0@gmail.com>
2025-08-14 20:29:03 +0300

    Toggle new task form on button click

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
+var NewTaskButton = document.getElementById("new-task-btn");
+var New = document.getElementById("new");
 
+function toggleForm() {
+  New.classList.toggle("hide");
+
+ 
+}
+
+NewTaskButton.addEventListener("click", toggleForm);
