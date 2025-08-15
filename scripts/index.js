7211842bc2dd4e064332c0d5456e5f5b9d516aa1
reem moustapha <reemmoustapha0@gmail.com>
2025-08-15 09:06:01 +0300

    Added edit and delete buttons to the tasks

--- scripts/index.js
+++ scripts/index.js
@@ -14,15 +14,23 @@ var taskInput = document.getElementById('task-input');
 var addTaskBtn = document.getElementById('add-task-btn');
 var taskList = document.getElementById('task-list');
 
+
 function addTask(event) {
     event.preventDefault();
     var taskText = taskInput.value.trim();
-    if (!taskText) return;
+    if (!taskText) {
+        
+        return;
+    }
 
+    
     var li = document.createElement('li');
     li.innerHTML =`
     <input type ="checkbox" class="checkbox">
     <span>${taskText}</span>
+    <div class="task-buttons">
+        <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
+        <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
     `;
     
     taskList.appendChild(li);
